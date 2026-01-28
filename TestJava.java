3a5fbe4d5ce10fc324cfb806698a658be96598f6
md7762102-a11y <md7762102@gmail.com>
2026-01-28 17:32:45 +0530

    Rename TestJava method to main

--- TestJava.java
+++ TestJava.java
@@ -1,5 +1,5 @@
 public class TestJava {
-public static void TestJava(String[] args) {
+public static void main(String[] args) {
 System.out.println("Hello World!"); // Prints with a newline
 System.out.print("Hello "); // Prints without newline
 System.out.print("World!");
