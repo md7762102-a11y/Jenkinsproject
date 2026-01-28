8a2a5b87e6c67b51e1ff844786711e4409428ef9
md7762102-a11y <md7762102@gmail.com>
2026-01-28 17:09:00 +0530

    Add TestJava.java with Hello World example

---
+++ TestJava.java
@@ -0,0 +1,7 @@
+public class Main {
+public static void main(String[] args) {
+System.out.println("Hello World!"); // Prints with a newline
+System.out.print("Hello "); // Prints without newline
+System.out.print("World!");
+}
+}
